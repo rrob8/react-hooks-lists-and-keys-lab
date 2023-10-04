f63ab17134efd1bf9b64f5631fa4a308e3610cb5
Robert Bubnik <rbubnik@gmail.com>
2023-10-03 20:30:21 -0400

    passing tests

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,11 +2,15 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
+
+  const allProjects = projects.map((project) => {
+    return <ProjectItem key={project.name} name={project.name} about={project.about} technologies={project.technologies}/>
+  })
+ 
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{allProjects}</div>
     </div>
   );
 }
